31d4d7479b33e96b4d6e20b656166f17b4858884
Manish Premi <manish.premi@fplabs.tech>
2024-12-25 23:16:23 +0530

    deployment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dualknight71752271": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s34a5dfb46": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:dual_knights/dual_knights.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final AmplifyAuthCognito auth = AmplifyAuthCognito();
+  final AmplifyStorageS3 storage = AmplifyStorageS3();
+
+  await Amplify.addPlugins([auth, storage]);
+    try {
+  await Amplify.configure('your amplifyconfiguration.json');
+  } catch (e) {
+    print('Error configuring Amplify: $e');
+  }
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 
