35e0256421f6fba185ee4f63a2db3d7a30a65e88
Sainath Latkar <latkarsainath@gmail.com>
2024-12-29 13:34:44 +0530

    [Add] changes for Game open UI

---
+++ assets/tiles/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="base" tilewidth="64" tileheight="64" tilecount="1683" columns="51">
+ <image source="../images/base_image/base.png" width="3296" height="2144"/>
+</tileset>
