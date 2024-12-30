86a18e5fe55bd8d1f585f467879071f298edf6ab
Sainath Latkar <latkarsainath@gmail.com>
2024-12-30 19:49:53 +0530

    [Add] changs for Tiled

---
+++ assets/tiles/UI/StarYellow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="StarYellow" tilewidth="41" tileheight="40" tilecount="1" columns="1">
+ <image source="../../images/Yellow/StarYellow.png" width="41" height="40"/>
+</tileset>
