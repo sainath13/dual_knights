bf05cce9d6e4ab94a956b2e70682a186c614d661
Sainath Latkar <latkarsainath@gmail.com>
2024-12-30 22:32:29 +0530

    [Add] changes for level completion screen

---
+++ assets/tiles/3G_Idle_(NoShadow)-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="3G_Idle_(NoShadow)-sheet" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="../images/Resources/Resources/3G_Idle_(NoShadow)-sheet.png" width="128" height="128"/>
+</tileset>
