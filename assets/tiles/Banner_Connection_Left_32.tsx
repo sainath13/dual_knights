ca989cdeffd8bd583e00f372abb9475e67a8f281
Sainath Latkar <latkarsainath@gmail.com>
2024-12-31 18:58:32 +0530

    [Add] changes for UI

---
+++ assets/tiles/Banner_Connection_Left_32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Banner_Connection_Left_32" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../images/UI/Banners/Banner_Connection_Left.png" width="192" height="192"/>
+</tileset>
---
+++ assets/tiles/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="background" tilewidth="64" tileheight="64" tilecount="475" columns="25">
+ <image source="../../../background.png" width="1600" height="1216"/>
+</tileset>
