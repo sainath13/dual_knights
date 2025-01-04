a18c199bbffba76479ca037a1324a9bc71c08e0e
Sainath Latkar <latkarsainath@gmail.com>
2025-01-04 14:16:34 +0530

    [Add] changes for Open Game screen

--- assets/tiles/HappySheep_Idle.tsx
+++ assets/tiles/HappySheep_Idle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="HappySheep_Idle" tilewidth="64" tileheight="64" tilecount="32" columns="16">
+<tileset version="1.10" tiledversion="1.11.0" name="HappySheep_Idle" tilewidth="128" tileheight="128" tilecount="8" columns="8">
  <image source="../images/Resources/Sheep/HappySheep_Idle.png" width="1024" height="128"/>
 </tileset>
