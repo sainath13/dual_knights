e6d73658beb2d45b37349cf22e1f3c969892c0e4
Sainath Latkar <latkarsainath@gmail.com>
2025-01-04 14:27:36 +0530

    [Add] changes for open screen

---
+++ assets/tiles/Warrior_Blue_Big.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Warrior_Blue_Big" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../images/Factions/Knights/Troops/Warrior/Blue/Warrior_Blue_Big.png" width="512" height="512"/>
+</tileset>
---
+++ assets/tiles/Warrior_Red_Big.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Warrior_Red_Big" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../images/Factions/Knights/Troops/Warrior/Red/Warrior_Red_Big.png" width="512" height="512"/>
+</tileset>
