7f47016268d441219fad712d2ad542113b7b4f7c
Sainath Latkar <latkarsainath@gmail.com>
2025-01-04 14:12:07 +0530

    [Add] changes for Game open screen

--- assets/tiles/Tree_192.tsx
+++ assets/tiles/Tree_192.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Tree_192" tilewidth="192" tileheight="192" tilecount="12" columns="4">
  <image source="../images/Resources/Trees/Tree.png" width="768" height="576"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
