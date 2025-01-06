5e4ac2e2cc9bf268cb3959773b559bb206c62364
Sainath Latkar <latkarsainath@gmail.com>
2025-01-06 18:41:10 +0530

    [Add] changes for UI

---
+++ assets/tiles/Dead.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Dead" tilewidth="64" tileheight="64" tilecount="56" columns="14">
+ <image source="../images/Factions/Knights/Troops/Dead/Dead.png" width="896" height="256"/>
+</tileset>
